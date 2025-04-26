4be388c86f2b466ce9bd5659319a7bb59d98c8d8
Nihal Chowdhury <nihal.c614@gmail.com>
2025-04-26 12:13:32 -0700

    Reset Everything

--- Makefile
+++
@@ -1,6 +0,0 @@
-CC=gcc
-
-SRC := $(wildcard src/*.c)
-
-run:
-	gcc -Iinc -o czg $(wildcard src/*c)
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-gcc -o czg main.c lexer.c file.c token.c container.c parser.c
